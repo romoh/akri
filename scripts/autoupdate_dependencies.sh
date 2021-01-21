c4b748e338abe97cb689b7df8802fa3a8d054864
romoh <romoh@microsoft.com>
2021-01-21 00:16:53 -0800

    Auto-update cargo crates

--- scripts/autoupdate_dependencies.sh
+++ scripts/autoupdate_dependencies.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-GITHUB_TOKEN="ad93a3fb57879bf14d0b6ea497102cc60efcabc7"
+GITHUB_TOKEN="7713e7d2ee6fa5fde1f1b55f6af50727e46fb46f"
 EMAIL="romoh@microsoft.com"
 USERNAME="romoh"
+ORGANIZATION="romoh" #"deislabs"
 
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "GITHUB_TOKEN is not defined"
     exit 1
 fi
 
-BRANCH_NAME="automated_cargo_update"
+BRANCH_NAME="automated_cargo_update_"
 
 # assumes the repo is already cloned as a prerequisite for running the script
 git checkout -b $BRANCH_NAME
@@ -22,15 +23,17 @@ then
     git config --global user.email $EMAIL
     git config --global user.name $USERNAME
 
-    # format: https://[USERNAME]:[TOKEN]@github.com/[USERNAME]/[REPO].git
-    git remote add authenticated https://romoh:$GITHUB_TOKEN@github.com/romoh/akri.git
+    # format: https://[USERNAME]:[TOKEN]@github.com/[ORGANIZATION]/[REPO].git
+    git remote add authenticated https://$USERNAME:$GITHUB_TOKEN@github.com/$ORGANIZATION/akri.git
 
     # commit the changes to Cargo.lock
     git commit -a -m "Auto-update cargo crates"
+    
+    # push the changes
     git push authenticated $BRANCH_NAME
 
     # finally create the PR
     curl -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" \
          --data '{"title":"Auto-update cargo crates","head":"automated_cargo_update","base":"main", "body":"Dependencies update review"}' \
-         https://api.github.com/repos/romoh/akri/pulls
+         https://api.github.com/repos/$ORGANIZATION/akri/pulls
 fi
