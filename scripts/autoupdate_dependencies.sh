d7a912558690ef085dcd7be9bb6c5651d088eb61
romoh <romoh@microsoft.com>
2021-01-21 00:04:31 -0800

    Auto-update cargo crates

--- scripts/autoupdate_dependencies.sh
+++ scripts/autoupdate_dependencies.sh
@@ -11,40 +11,26 @@ fi
 
 BRANCH_NAME="automated_cargo_update"
 
+# assumes the repo is already cloned as a prerequisite for running the script
 git checkout -b $BRANCH_NAME
-cargo update #&& cargo test
+cargo update && cargo test
 
 DIFF=`git diff`
-if [ -n "$DIFF" ]; then
+if [ -n "$DIFF" ]
+then
     # configure git authorship
     git config --global user.email $EMAIL
     git config --global user.name $USERNAME
 
-    git remote add authenticated https://romoh:$GITHUB_TOKEN@github.com/romoh/akri.git #https://github.com/romoh/akri.git
+    # format: https://[USERNAME]:[TOKEN]@github.com/[USERNAME]/[REPO].git
+    git remote add authenticated https://romoh:$GITHUB_TOKEN@github.com/romoh/akri.git
 
     # commit the changes to Cargo.lock
     git commit -a -m "Auto-update cargo crates"
-
-    # push the changes so that PR API has something to compare against
     git push authenticated $BRANCH_NAME
 
     # finally create the PR
     curl -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" \
          --data '{"title":"Auto-update cargo crates","head":"automated_cargo_update","base":"main", "body":"Dependencies update review"}' \
          https://api.github.com/repos/romoh/akri/pulls
-
-    # # add a remote with read/write permissions!
-    # # use token authentication instead of password
-    # git remote add authenticated https:/romoh/akri:$GITHUB_TOKEN@github.com/romoh/akri.git
-    
-    # # commit the changes to Cargo.lock
-    # git commit -a -m "Auto-update cargo crates"
-
-    # # push the changes so that PR API has something to compare against
-    # git push authenticated $BRANCH_NAME
-
-    # # finally create the PR
-    # curl -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" \
-    #      --data '{"title":"Auto-update cargo crates","head":"automated_cargo_update","base":"master", "body":"@atodorov review"}' \
-    #      https://github.com/romoh/akri/pulls
 fi
